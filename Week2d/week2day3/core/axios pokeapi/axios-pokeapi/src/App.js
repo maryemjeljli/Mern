450ecb796dbed1dd486cfc37e87f8d62d33a8e01
unknown <mjeljli27@gmail.com>
2023-08-28 10:18:55 +0200

    second commit

---
+++ Week2d/week2day3/core/axios pokeapi/axios-pokeapi/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useEffect,useState} from 'react';
+
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0     ')
+    .then(res=>{ res.data.results 
+    setpokeapi(res.data.results)})
+  })
+    .catch((err)=>{
+      console.log(err);
+    },[])
+
+  return (
+    <div className="App">
+      <ul>
+        {
+          pokemon.map((pokemonapi, index)=>{
+            return (<li key={index}>
+                {pokemonapi.name}</li>)
+          })
+        }
+      </ul>
+      
+    </div>
+      
+    
+  );
+}
+
+export default App;
